9f26c7219006f81777c6b248aa56b56d031bb7fc
eipiguy <ndnesbitt@gmail.com>
2022-05-21 07:39:08 -0500

    Basic framework. Can't tests to find the library yet

--- nlinear/headers/vector.hxx
+++ nlinear/headers/vector.hxx
@@ -1,41 +1,134 @@
 #include "nlinear_export.h"
 #include <initializer_list>
 
+using std::size_t;
+
 ///////////////////////////////////////////////////////////////////////////////
 // Vector
 
 // Generic Vector
-template <typename T, int n>
+template <typename T, size_t n>
 struct NLINEAR_EXPORT Vector {
     T data[n];
 
     // Constructors
-    constexpr explicit Vector<T, n> (const T fill);
-    constexpr explicit Vector<T, n> (const std::initializer_list<T> list);
+    constexpr explicit Vector (const T &fill);
+    constexpr explicit Vector (const std::initializer_list<T> &list);
 
-    // Component-Wise Operations
-    Vector<T,n> operator+ (const Vector<T,n> right);
-    Vector<T,n> operator- (const Vector<T,n> right);
+    // Assignment & Access
+    void operator= (const Vector<T,n> &right);
+    T operator[] (const size_t i);
 
-    // Vector Specific Operations
-    constexpr T length();
-    constexpr void normalize();
+    // Component-Wise
+    Vector<T,n> operator+ (const Vector<T,n> &right);
+    Vector<T,n> operator- (const Vector<T,n> &right);
+    Vector<T,n> operator* (const Vector<T,n> &right);
+    Vector<T,n> operator/ (const Vector<T,n> &right);
+    Vector<T,n> operator% (const Vector<T,n> &right);
+
+    void operator+= (const Vector<T,n> &right);
+    void operator-= (const Vector<T,n> &right);
+    void operator*= (const Vector<T,n> &right);
+    void operator/= (const Vector<T,n> &right);
+    void operator%= (const Vector<T,n> &right);
+
+    Vector<T,n> abs();
+
+    // Scalar From Right
+    Vector<T,n> operator+ (const T &right);
+    Vector<T,n> operator- (const T &right);
+    Vector<T,n> operator* (const T &right);
+    Vector<T,n> operator/ (const T &right);
+    Vector<T,n> operator% (const T &right);
+
+    // Comparison
+    Vector<bool,n> operator== (const Vector<T,n> &right);
+    Vector<bool,n> operator!= (const Vector<T,n> &right);
+    Vector<bool,n> operator< (const Vector<T,n> &right);
+    Vector<bool,n> operator<= (const Vector<T,n> &right);
+    Vector<bool,n> operator> (const Vector<T,n> &right);
+    Vector<bool,n> operator>= (const Vector<T,n> &right);
+
+    // Utility
+    auto length ();
+    void normalize ();
+
+    T sum();
+    T product();
+
+    T min(size_t *minID=nullptr);
+    T max(size_t *maxID=nullptr);
+
+    bool all();
+    bool any();
 };
 
 //=============================================================================
-// Vector-Based Functions
+// Scalar Operators from Left
 
-template <typename T, int n>
-NLINEAR_EXPORT T dot(const Vector<T, n> left, const Vector<T, n> right);
+template <typename T, size_t n>
+Vector<T,n> operator+ (const T &left, const Vector<T,n> &right);
 
-template <typename T, int n>
-NLINEAR_EXPORT Vector<T, n> cross(const Vector<T, n> left, const Vector<T, n> right);
+template <typename T, size_t n>
+Vector<T,n> operator- (const T &left, const Vector<T,n> &right);
+
+template <typename T, size_t n>
+Vector<T,n> operator* (const T &left, const Vector<T,n> &right);
+
+template <typename T, size_t n>
+Vector<T,n> operator/ (const T &left, const Vector<T,n> &right);
+
+template <typename T, size_t n>
+Vector<T,n> operator% (const T &left, const Vector<T,n> &right);
+
+//=============================================================================
+//Component-Wise Non-Operators
+
+template <typename T, size_t n>
+Vector<T,n> min(const Vector<T,n> &left, const Vector<T,n> &right);
+
+template <typename T, size_t n>
+Vector<T,n> min(const T &left, const Vector<T,n> &right);
+
+template <typename T, size_t n>
+Vector<T,n> min(const Vector<T,n> &left, const T &right);
+
+//-----------------------------------------------------------------------------
+
+template <typename T, size_t n>
+Vector<T,n> max(const Vector<T,n> &left, const Vector<T,n> &right);
+
+template <typename T, size_t n>
+Vector<T,n> max(const T &left, const Vector<T,n> &right);
+
+template <typename T, size_t n>
+Vector<T,n> max(const Vector<T,n> &left, const T &right);
+
+//-----------------------------------------------------------------------------
+
+template <typename T, size_t n>
+Vector<T,n> clamp(const Vector<T,n> &v, const Vector<T,n> &min, const Vector<T,n> &max);
+
+template <typename T, size_t n>
+Vector<T,n> clamp(const Vector<T,n> &v, const T &min, const Vector<T,n> &max);
+
+template <typename T, size_t n>
+Vector<T,n> clamp(const Vector<T,n> &v, const Vector<T,n> &min, const T &max);
+
+template <typename T, size_t n>
+Vector<T,n> clamp(const Vector<T,n> &v, const T &min, const T &max);
+
+//=============================================================================
+// Vector-Based Free Functions
+
+template <typename T, size_t n>
+NLINEAR_EXPORT T dot (const Vector<T,n> &left, const Vector<T,n> &right);
 
 ///////////////////////////////////////////////////////////////////////////////
 // Specializations for 2, 3, and 4 Dimensions
 
 template <typename T>
-struct NLINEAR_EXPORT Vector<T, 2> {
+struct NLINEAR_EXPORT Vector<T,2> {
     union {
         T data[2];
         struct { T x, y; };
@@ -43,34 +136,37 @@ struct NLINEAR_EXPORT Vector<T, 2> {
 };
 
 template <typename T>
-struct NLINEAR_EXPORT Vector<T, 3> {
+struct NLINEAR_EXPORT Vector<T,3> {
     union {
         T data[3];
         struct { T x, y, z; };
         struct { T r, g, b; };
-        Vector<T, 2> xy;
+        Vector<T,2> xy;
     };
 };
 
 template <typename T>
-struct NLINEAR_EXPORT Vector<T, 4> {
+NLINEAR_EXPORT Vector<T,3> cross (const Vector<T,3> left, const Vector<T,3> right);
+
+template <typename T>
+struct NLINEAR_EXPORT Vector<T,4> {
     union {
         T data[4];
         struct { T x, y, z, w; };
         struct { T r, g, b, a; };
-        Vector<T, 2> xy;
-        Vector<T, 3> xyz;
-        Vector<T, 3> rgb;
+        Vector<T,2> xy;
+        Vector<T,3> xyz;
+        Vector<T,3> rgb;
     };
 };
 
 ///////////////////////////////////////////////////////////////////////////////
 // Typedefs for Ease of Use
 
-typedef Vector<float, 2> float2;
-typedef Vector<float, 3> float3;
-typedef Vector<float, 4> float4;
+typedef Vector<float,2> float2;
+typedef Vector<float,3> float3;
+typedef Vector<float,4> float4;
 
-typedef Vector<double, 2> double2;
-typedef Vector<double, 3> double3;
-typedef Vector<double, 4> double4;
+typedef Vector<double,2> double2;
+typedef Vector<double,3> double3;
+typedef Vector<double,4> double4;
--- nlinear/src/vector.cxx
+++ nlinear/src/vector.cxx
@@ -1,4 +1,344 @@
 #include "vector.hxx"
+#include <cmath>
 
 ///////////////////////////////////////////////////////////////////////////////
-// Vector
+// Generic Vector
+
+//=============================================================================
+// Constructors
+
+template <typename T, size_t n>
+constexpr Vector<T,n> :: Vector (const T &fill) {
+    for( T d : data ){
+        d = fill;
+    }
+    return;
+}
+
+template <typename T, size_t n>
+constexpr Vector<T,n> :: Vector (const std::initializer_list<T> &list)
+    : data(list) {}
+
+//=============================================================================
+// Assignment & Access
+
+template <typename T, size_t n>
+void Vector<T,n> :: operator= (const Vector<T,n> &right){
+    data = right.data;
+}
+
+template <typename T, size_t n>
+T Vector<T,n> :: operator[] (size_t i){
+    return data[i];
+}
+
+//=============================================================================
+// Component-Wise
+
+template <typename T, size_t n>
+Vector<T,n> Vector<T,n> :: operator+ (const Vector<T,n> &right){
+    Vector<T,n> out;
+    for(size_t i=0; i<n; ++i){
+        out.data[i] = data[i] + right.data[i];
+    }
+    return out;
+}
+
+template <typename T, size_t n>
+Vector<T,n> Vector<T,n> :: operator- (const Vector<T,n> &right){
+    Vector<T,n> out;
+    for(size_t i=0; i<n; ++i){
+        out.data[i] = data[i] - right.data[i];
+    }
+    return out;
+}
+
+template <typename T, size_t n>
+Vector<T,n> Vector<T,n> :: operator* (const Vector<T,n> &right){
+    Vector<T,n> out;
+    for(size_t i=0; i<n; ++i){
+        out.data[i] = data[i] * right.data[i];
+    }
+    return out;
+}
+
+template <typename T, size_t n>
+Vector<T,n> Vector<T,n> :: operator/ (const Vector<T,n> &right){
+    Vector<T,n> out;
+    for(size_t i=0; i<n; ++i){
+        out.data[i] = data[i] / right.data[i];
+    }
+    return out;
+}
+
+template <typename T, size_t n>
+Vector<T,n> Vector<T,n> :: operator% (const Vector<T,n> &right){
+    Vector<T,n> out;
+    for(size_t i=0; i<n; ++i){
+        out.data[i] = data[i] % right.data[i];
+    }
+    return out;
+}
+
+//-----------------------------------------------------------------------------
+
+template <typename T, size_t n>
+void Vector<T,n> :: operator+= (const Vector<T,n> &right){
+    for(size_t i=0; i<n; ++i){
+        data[i] += right.data[i];
+    }
+    return;
+}
+
+template <typename T, size_t n>
+void Vector<T,n> :: operator-= (const Vector<T,n> &right){
+    for(size_t i=0; i<n; ++i){
+        data[i] -= right.data[i];
+    }
+    return;
+}
+
+template <typename T, size_t n>
+void Vector<T,n> :: operator*= (const Vector<T,n> &right){
+    for(size_t i=0; i<n; ++i){
+        data[i] *= right.data[i];
+    }
+    return;
+}
+
+template <typename T, size_t n>
+void Vector<T,n> :: operator/= (const Vector<T,n> &right){
+    for(size_t i=0; i<n; ++i){
+        data[i] /= right.data[i];
+    }
+    return;
+}
+
+template <typename T, size_t n>
+void Vector<T,n> :: operator%= (const Vector<T,n> &right){
+    for(size_t i=0; i<n; ++i){
+        data[i] %= right.data[i];
+    }
+    return;
+}
+
+//-----------------------------------------------------------------------------
+
+template <typename T, size_t n>
+Vector<T,n> Vector<T,n> :: abs () {
+    Vector<T,n> out;
+    for( size_t i=0; i<n; ++i; ){
+        out.data[i] = abs( d[i] );
+    }
+}
+
+//=============================================================================
+// Scalar From Right
+
+template <typename T, size_t n>
+Vector<T,n> Vector<T,n> :: operator+ (const T &right){
+    Vector<bool,n> out;
+    for( size_t i=0; i<n; ++i ){
+        out.data = data[i] + right;
+    }
+    return out;
+}
+
+template <typename T, size_t n>
+Vector<T,n> Vector<T,n> :: operator- (const T &right){
+    Vector<bool,n> out;
+    for( size_t i=0; i<n; ++i ){
+        out.data = data[i] - right;
+    }
+    return out;
+}
+
+template <typename T, size_t n>
+Vector<T,n> Vector<T,n> :: operator* (const T &right){
+    Vector<bool,n> out;
+    for( size_t i=0; i<n; ++i ){
+        out.data = data[i] * right;
+    }
+    return out;
+}
+
+template <typename T, size_t n>
+Vector<T,n> Vector<T,n> :: operator/ (const T &right){
+    Vector<bool,n> out;
+    for( size_t i=0; i<n; ++i ){
+        out.data = data[i] / right;
+    }
+    return out;
+}
+
+template <typename T, size_t n>
+Vector<T,n> Vector<T,n> :: operator% (const T &right){
+    Vector<bool,n> out;
+    for( size_t i=0; i<n; ++i ){
+        out.data = data[i] % right;
+    }
+    return out;
+}
+
+//=============================================================================
+// Comparison
+
+template <typename T, size_t n>
+Vector<bool,n> Vector<T,n> :: operator== (const Vector<T,n> &right){
+    Vector<bool,n> out;
+    for( size_t i=0; i<n; ++i ){
+        out.data = ( data[i] == right.data[i] );
+    }
+    return out;
+}
+
+template <typename T, size_t n>
+Vector<bool,n> Vector<T,n> :: operator!= (const Vector<T,n> &right){
+    Vector<bool,n> out;
+    for( size_t i=0; i<n; ++i ){
+        out.data = ( data[i] != right.data[i] );
+    }
+    return out;
+}
+
+template <typename T, size_t n>
+Vector<bool,n> Vector<T,n> :: operator< (const Vector<T,n> &right){
+    Vector<bool,n> out;
+    for( size_t i=0; i<n; ++i ){
+        out.data = ( data[i] < right.data[i] );
+    }
+    return out;
+}
+
+template <typename T, size_t n>
+Vector<bool,n> Vector<T,n> :: operator<= (const Vector<T,n> &right){
+    Vector<bool,n> out;
+    for( size_t i=0; i<n; ++i ){
+        out.data = ( data[i] <= right.data[i] );
+    }
+    return out;
+}
+
+template <typename T, size_t n>
+Vector<bool,n> Vector<T,n> :: operator> (const Vector<T,n> &right){
+    Vector<bool,n> out;
+    for( size_t i=0; i<n; ++i ){
+        out.data = ( data[i] > right.data[i] );
+    }
+    return out;
+}
+
+template <typename T, size_t n>
+Vector<bool,n> Vector<T,n> :: operator>= (const Vector<T,n> &right){
+    Vector<bool,n> out;
+    for( size_t i=0; i<n; ++i ){
+        out.data = ( data[i] >= right.data[i] );
+    }
+    return out;
+}
+
+//=============================================================================
+// Utility
+
+template <typename T, size_t n>
+auto Vector<T,n> :: length (){
+    return sqrt( dot(this,this) );
+}
+
+template <typename T, size_t n>
+void Vector<T,n> :: normalize (){
+    auto l = this.length();
+    for( T d : data ){
+        d /= l;
+    }
+    return;
+}
+
+//-----------------------------------------------------------------------------
+
+template <typename T, size_t n>
+T Vector<T,n> :: sum(){
+    T total = 0;
+    for( T d : data ){
+        total += d;
+    }
+    return total;
+}
+
+template <typename T, size_t n>
+T Vector<T,n> :: product(){
+    T total = 1;
+    for( T d : data ){
+        total *= d;
+    }
+    return total;
+}
+
+template <typename T, size_t n>
+size_t Vector<T,n> :: min(size_t *minID){
+    if(minID){
+        *minID = 0;
+    }
+    T m = data[0];
+    for( size_t i=0; i<n; ++i ){
+        if( data[i] < m ){
+            m = data[i];
+            if(minID){
+                *minID = i;
+            }
+        }
+    }
+    return m;
+}
+
+template <typename T, size_t n>
+T Vector<T,n> :: max(size_t *maxID){
+    if(maxID){
+        *maxID = 0;
+    }
+    T m = data[0];
+    for( size_t i=0; i<n; ++i ){
+        if( d > m ){
+            m = d;
+            if(maxID){
+                *maxID = i;
+            }
+        }
+    }
+    return m;
+}
+
+//-----------------------------------------------------------------------------
+
+template <typename T, size_t n>
+bool Vector<T,n> :: all(){
+    for( T d : data ){
+        if( !d ){
+            return false;
+        }
+    }
+    return true;
+}
+
+template <typename T, size_t n>
+bool Vector<T,n> :: any(){
+    for( T d : data ){
+        if( d ){
+            return true;
+        }
+    }
+    return false;
+}
+
+//=============================================================================
+// Scalar Operators from Left
+
+//=============================================================================
+//Component-Wise Non-Operators
+
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+
+//=============================================================================
+// Vector-Based Free Functions
